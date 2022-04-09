933b6c50bd96c1ee9e9d0eccd68cbe2911f9d9ab
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-04-09 14:56:49 +0530

    Added code for notes app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# node-notes-app
+# Node notes app
+---
+
+### Take notes using node command line
+#### Use node command line to create, read and delete notes
+
+---
+
+## How to run the code?
+
+* run << npm i >>
+* run << node app.js --help >> to get all the custom command
+
---
+++ notes.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+const chalk = require('chalk')
+
+
+const loadNotes = () => {
+  try {
+    let dataBuffer = fs.readFileSync("notes.json");
+    let jsonData = dataBuffer.toString();
+    let data = JSON.parse(jsonData);
+    return data;
+  } catch (error) {
+    console.log('inside catch block');
+    return [];
+  }
+};
+
+const addNote = (title, body) => {
+  let notes = loadNotes();
+let isDuplicate = notes.find(note=>note.title===title)
+if(isDuplicate){
+       console.log(chalk.red('Title already exists. Try a different one !'))
+       return 
+}
+
+  notes.push({
+    title: title,
+    body: body,
+  });
+
+  saveNote(notes);
+  console.log(chalk.green('Note added succesfully!'))
+};
+
+const saveNote = (notes) => {
+  let data = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", data);
+};
+
+const removeNote =(title) =>{
+  let notes = loadNotes()
+    let notesToBeRetained =  notes.filter(note=>note.title!==title)
+    if(notes.length===notesToBeRetained.length){
+      console.log(chalk.inverse.red('Title not found!'))
+      return
+    }
+  saveNote(notesToBeRetained);
+  console.log(chalk.inverse.magenta('Note removed succesfully!'))
+}
+
+const listNotes=()=>{
+  let i=1
+  let notes = loadNotes()
+  console.log(chalk.blue.inverse('Your Notes\n'))
+  notes.map(note=>{
+    console.log(`${i}. ${note.title}\n`)
+    i++
+  })
+}
+
+const readNotes = (title) =>{
+  let notes = loadNotes()
+  let noteToBeDisplayed = notes.find(note=>note.title==title)
+  if(noteToBeDisplayed===undefined){
+    console.log(chalk.red('No note found, please check your title'))
+    return
+  }
+  console.log(chalk.bold.underline.green(noteToBeDisplayed.title))
+  console.log(`${chalk.blue(noteToBeDisplayed.body)}`)
+}
+
+module.exports = {  addNote,removeNote,listNotes,readNotes };
